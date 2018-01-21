d5e29d085b27f44ed35de4b2360dec7d0aabf27d
Овчиников Анатолий <aovchinnikov@MacBook-Pro-Ovcinikov.local>
2018-01-21 18:03:38 +0300

    feat/ add Header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
-)
+import Header from "./Header";
+
+const App = () => <Header />;
 
 export default App;
--- src/Header/index.js
+++ src/Header/index.js
@@ -1,1 +1,66 @@
-import React, { Component } from 'react';
+import React from "react";
+import styled from "styled-components";
+
+import logo from "./assets/AO_logo.svg";
+import behance from "./assets/behance.svg";
+import instagram from "./assets/instagram.svg";
+
+const Header = styled.header`
+  border-top: 1px solid;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  width: 10rem;
+`;
+
+const Navigation = styled.nav`
+  vertical-align: bottom;
+`;
+
+const Link = styled.a`
+  margin-left: 2rem;
+  color: #000;
+  text-decoration: none;
+
+  &:hover {
+    color: #636363;
+  }
+`;
+
+const Icon = styled.img`
+  width: 1.25rem;
+  vertical-align: -4px;
+`;
+
+const About = styled.p``;
+
+export default () => (
+  <Header>
+    <div className="container">
+      <Wrapper>
+        <Logo src={logo} />
+        <Navigation>
+          <Link href="#">Портфолио</Link>
+          <Link href="#">Профиль</Link>
+          <Link href="#">Контакты</Link>
+          <Link href="#">
+            <Icon src={behance} />
+          </Link>
+          <Link href="#">
+            <Icon src={instagram} />
+          </Link>
+        </Navigation>
+      </Wrapper>
+      <About>
+        Многопрофильный художник и дизайнер с акцентом на индивидуальный <br />
+        брендинг и типографию.
+      </About>
+    </div>
+  </Header>
+);
